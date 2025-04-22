439fd8c0c46a736b40661789644919d623696c14
Sridhar-Ghr <sridhar.cisms@gmail.com>
2025-04-22 11:55:27 +0530

    Added logic to the controller to get order confirmation and also modified the html home page

--- controller.js
+++ controller.js
@@ -2,6 +2,7 @@
 
 const http=require('http');
 const fs=require('fs');
+const querystring=require('querystring');
 
 const server=http.createServer((req,res)=>{
    if(req.url==='/'){
@@ -15,6 +16,25 @@ const server=http.createServer((req,res)=>{
         res.end(data);
        }
     });
+   }
+   else if(req.url==='/orderconfirmation'){
+      let body='';
+      req.on('data',chunk=>{
+         body+=chunk.toString();
+      });
+
+      req.on('end',()=>{
+         const postReqData=querystring.parse(body);
+         const ordersummary=postReqData;
+
+         res.writeHead(200,{'Content-Type':'text/html'});
+         res.end(`<html><body>The order summary is: <br><p>${JSON.stringify(ordersummary)}</p></body></html>`);
+      });
+   }
+
+   else{
+      res.writeHead(500,{'Content-Type':'text/html'});
+      res.end('Please verify your server, it seems there is an error');
    }
 
 });
