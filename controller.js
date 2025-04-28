4dc86249524696e9e8bdbb0bd16d7ff903c4d034
Sridhar-Ghr <sridhar.cisms@gmail.com>
2025-04-28 14:24:40 +0530

    added code to write the confirmed orders to a file

--- controller.js
+++ controller.js
@@ -3,6 +3,7 @@
 const http=require('http');
 const fs=require('fs');
 const querystring=require('querystring');
+const orderHistoryFilePath='D:\\Sridhar\\JNTUH-RS\\Fullstack_Development\\Projects\\Food-Delivery-App\\orderHistory.txt';
 
 const server=http.createServer((req,res)=>{
    if(req.url==='/'){
@@ -27,9 +28,35 @@ const server=http.createServer((req,res)=>{
          const postReqData=querystring.parse(body);
          const ordersummary=postReqData;
          const {orderSummary}=ordersummary;
+         const fileContent=orderSummary;
 
          res.writeHead(200,{'Content-Type':'text/html'});
          res.end(`<html><body>The order summary is: <br><p style="display:flex; align="center"">${orderSummary}</p></body></html>`);
+         
+         //Open the Order history file and write the current order to file
+
+         fs.open(orderHistoryFilePath,'w',(err,fd)=>{
+            if(err)
+               console.log('There is some issue while accessing the file',err);
+            
+            //Write to the file
+            fs.writeFile(fd,fileContent,(err,written,string)=>{
+               if(err)
+                  console.log('Unable to write to the file',err);
+               else{
+                  console.log('Order history File has been updated');
+                  console.log(written,string);
+               }
+            //Close the file after writing
+             fs.close(fd,(err)=>{
+               if(err)
+                  console.log('Unable to close the file');
+               else
+                  console.log('File has been closed successfully');
+             });
+            });
+
+         });
       });
    }
 
